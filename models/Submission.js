9f637bb225fd1b815d1570ddbc1d11e561ef670b
riefer02 <andrew.riefenstahl@gmail.com>
2020-07-30 08:55:05 -0500

    new SEO meta data w/ vue-meta added

--- models/Submission.js
+++ models/Submission.js
@@ -1,55 +1,58 @@
 const mongoose = require("mongoose");
 
-const submissionSchema = new mongoose.Schema({
-	firstname: {
-		type: String,
-		required: [true, "Please provide a first name"],
-	},
-	lastname: {
-		type: String,
-		required: [true, "Please provide a last name"],
-	},
-	email: {
-		type: String,
-		required: [true, "Please provide a an email address"],
-		unique: true,
-	},
-	countryOfOrigin: {
-		type: String,
-		required: [true, "Please provide your country of origin"],
-	},
-	parentOneCountryOfOrigin: {
-		type: String,
-		required: [
-			true,
-			"Select an option for first parent's country of origin",
-		],
-	},
-	parentTwoCountryOfOrigin: {
-		type: String,
-		required: [
-			true,
-			"Select an option for second parent's country of origin",
-		],
-	},
-	userAge: {
-		type: Number,
-		required: [true, "Please move slider to provide your age"],
-	},
-	values: {
-		first: {
+const submissionSchema = new mongoose.Schema(
+	{
+		firstname: {
 			type: String,
-			required: [true, "Please select a first value"],
+			required: [true, "Please provide a first name"],
 		},
-		second: {
+		lastname: {
 			type: String,
-			required: [true, "Please select a second value"],
+			required: [true, "Please provide a last name"],
 		},
-		third: {
+		email: {
 			type: String,
-			required: [true, "Please select a third value"],
+			required: [true, "Please provide a an email address"],
+			unique: true,
+		},
+		countryOfOrigin: {
+			type: String,
+			required: [true, "Please provide your country of origin"],
+		},
+		parentOneCountryOfOrigin: {
+			type: String,
+			required: [
+				true,
+				"Select an option for first parent's country of origin",
+			],
+		},
+		parentTwoCountryOfOrigin: {
+			type: String,
+			required: [
+				true,
+				"Select an option for second parent's country of origin",
+			],
+		},
+		userAge: {
+			type: Number,
+			required: [true, "Please move slider to provide your age"],
+		},
+		values: {
+			first: {
+				type: String,
+				required: [true, "Please select a first value"],
+			},
+			second: {
+				type: String,
+				required: [true, "Please select a second value"],
+			},
+			third: {
+				type: String,
+				required: [true, "Please select a third value"],
+			},
 		},
 	},
-});
+	{ timestamps: { createdAt: "createdAt" } }
+);
 
 module.exports = mongoose.model("Submission", submissionSchema);
