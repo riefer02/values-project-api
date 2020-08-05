73719e516c14ce2fc77962a34aa65bfff060959d
riefer02 <andrew.riefenstahl@gmail.com>
2020-08-05 13:43:09 -0500

    Enhanced readibility and documentation

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
+// Environmental Configuration
 if (process.env.NODE_ENV === "development") {
 	require("dotenv").config({ path: "./config.env" });
 }
 
+// Initial Error Handling
 process.on("uncaughtException", (err) => {
 	console.log("UNCAUGHT EXCEPTION! 🔥 Shutting down...");
 	console.log(err.name, err.message);
 	process.exit(1);
 });
 
+// Project Environment
 const path = require("path");
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+
+// Middleware Packages
 const cors = require("cors");
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
@@ -19,32 +24,29 @@ const mongoSanitize = require("express-mongo-sanitize");
 const xss = require("xss-clean");
 const history = require("connect-history-api-fallback");
 
+// Logic 
 const AppError = require("./utils/appError");
 const submissionRouter = require("./routes/submissions");
 const dataRouter = require("./routes/data");
 const globalErrorHandler = require("./controllers/errorController");
-
 const publicPath = path.resolve(__dirname, "./public");
 
-// Limit Request From Same IP
+// GraphQL Implmentation 
+
+
+// Limiter for IP Requests
 const limiter = rateLimit({
 	max: 100,
 	windowMs: 60 * 60 * 1000,
 	message: "Too many requests from this IP!",
 });
-
 app.use("/api", limiter);
+app.use(history()); // SPA Application Requirement
+app.use(helmet()); // HTTP Security Headers
 
-app.use(history());
-
-// Set Security HTTP Headers
-app.use(helmet());
-
-console.log(process.env.NODE_ENV);
-
+// Database Establishment
 let port = process.env.PORT || 6969;
 if (port === null || port === "") port = 6969;
-
 mongoose
 	.connect(
 		process.env.MONGODB_URI ||
@@ -54,20 +56,16 @@ mongoose
 	.then((connection) => {
 		console.log(`DB connection successful`);
 	});
+app.use(express.json({ limit: "10kb" })); // Limit Incoming Requests Data
+app.use(mongoSanitize()); // Data Sanitization Against NoSQL Query Injection
+app.use(xss()); // Data Sanitation Against XXS
+app.use(cors()); // Open API Communication
 
-app.use(express.json({ limit: "10kb" }));
-
-// Data Sanitization Against NoSQL Query Injection
-app.use(mongoSanitize());
-
-// Data Sanitation Against XXS
-app.use(xss());
-
-app.use(cors());
-
+// Routing
 app.use("/api/v1/submit", submissionRouter);
 app.use("/api/v1/data", dataRouter);
 
+// Initializing Index
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(publicPath));
 	console.log(publicPath);
@@ -76,16 +74,18 @@ if (process.env.NODE_ENV === "production") {
 	});
 }
 
+// Global Error Handling
 app.all("*", (req, res, next) => {
 	next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
-
 app.use(globalErrorHandler);
 
+// Server Uplink
 const server = app.listen(port, () =>
 	console.log(`Server started on port: ${port}...`)
 );
 
+// Logic Error Handling
 process.on("unhandledRejection", (err) => {
 	console.log("UNHANDLED REJECTION! 🔥 Shutting down...");
 	console.log(err.name, err.message);
